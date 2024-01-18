20469d3cd7efcb8bbfbece3ba06cafcb405b9556
Vincent Klimjack <vklimjack@gmail.com>
2024-01-18 18:54:15 +0000

    update tables and move data to Repository layer

--- EnergyUsage.Database/Tables/EnergyAnomalies.sql
+++ EnergyUsage.Database/Tables/EnergyAnomalies.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[EnergyAnomalies]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Time] DATETIME NOT NULL, 
     [Consumption] DECIMAL(18,2) NOT NULL
 )
--- EnergyUsage.Database/Tables/Weather.sql
+++ EnergyUsage.Database/Tables/Weather.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Weather]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Date] DATETIME NOT NULL, 
     [Temperature] DECIMAL(5,2) NOT NULL, 
     [AverageHumidity] DECIMAL(3,2) NOT NULL
