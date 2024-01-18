018a0de7d3d089054a846efff67934ef0855a8ee
Vincent Klimjack <vklimjack@gmail.com>
2024-01-18 10:41:13 +0000

    upate weather table to use smaller decimal precision for temperature and humidity

--- EnergyUsage.Database/Tables/Weather.sql
+++ EnergyUsage.Database/Tables/Weather.sql
@@ -2,6 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Date] DATETIME NOT NULL, 
-    [Temperature] DECIMAL(18,2) NOT NULL, 
-    [AverageHumidity] DECIMAL(18,2) NOT NULL
+    [Temperature] DECIMAL(5,2) NOT NULL, 
+    [AverageHumidity] DECIMAL(3,2) NOT NULL
 )
